ca808b8185c22a244de9c09f989c2f359be5a1d3
Charles Luxinger <charles.luxinger@picpay.com>
2020-06-22 20:14:45 -0300

    Writing Files

--- Functions/functions.go
+++ Functions/functions.go
@@ -4,8 +4,10 @@ import (
 	"bufio"
 	"fmt"
 	"io"
+	"io/ioutil"
 	"net/http"
 	"os"
+	"strconv"
 	"strings"
 	"time"
 )
@@ -24,6 +26,7 @@ func main() {
 		iniciarMonitoramento()
 	case 2:
 		fmt.Println("Exibindo Logs...")
+		imprimeLogs()
 	case 0:
 		fmt.Println("Saindo do programa...")
 		os.Exit(0)
@@ -80,7 +83,7 @@ func testaSite(site string, i int) {
 	} else {
 		fmt.Println("Server", site, "Down, Status Code:", response.StatusCode)
 	}
-
+	registraLog(site, response.StatusCode)
 }
 
 func lerSitesDoArquivo() []string {
@@ -96,16 +99,40 @@ func lerSitesDoArquivo() []string {
 	reader := bufio.NewReader(file)
 
 	for {
-        line, err := reader.ReadString('\n')
-        
+		line, err := reader.ReadString('\n')
+
 		lines = append(lines, strings.TrimSpace(line))
 
 		if err != io.EOF {
 			break
 		}
-    }
-    
-    file.Close()
+	}
+
+	file.Close()
 
 	return lines
+}
+
+func registraLog(site string, statusCode int) {
+	file, err := os.OpenFile("log.txt", os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
+
+	if err != nil {
+		fmt.Println("Error:", err)
+		os.Exit(0)
+	}
+
+	file.WriteString(time.Now().Format("2006/01/02 15:04:05") + " - " + site + " - status code: " + strconv.Itoa(statusCode) + "\n")
+
+	file.Close()
+}
+
+func imprimeLogs(){
+	arquivo, err := ioutil.ReadFile("log.txt")
+
+	if err != nil {
+		fmt.Println("Error:", err)
+		os.Exit(0)
+	}
+
+	fmt.Println(string(arquivo))
 }
